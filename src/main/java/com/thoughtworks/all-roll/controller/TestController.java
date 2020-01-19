5c5d51b09a0d39de595d0173f296c091ad58a7c1
陈洁 <chenyu530324@163.com>
2020-01-19 16:56:08 +0800

    Init all-roll code base

---
+++ src/main/java/com/thoughtworks/all-roll/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.all-roll.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+}
